32e1b728dae4a9b7b1cb658144f8abec3dcc9fad
butaman <butaman_git@ebuta.net>
2013-11-01 20:19:19 +0900

    first commit

---
+++ OpapPsdSplitter/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhotoshopFile;
+using System.IO;
+
+namespace OpapPsdSplitter
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int files_start_pos = 0; //ファイル指定部の開始インデックス
+    
+            // 引数からスイッチを取得する
+            bool opt_recurse = false;
+            bool opt_opap_anime = false;
+            bool opt_show = false;
+            string opt_outdir_prefix = "out-";
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].StartsWith("-"))
+                {
+                    //TODO: もっと綺麗にする
+                    switch (args[i].Substring(1))
+                    {
+                        case "r":
+                            opt_recurse = true;
+                            break;
+                        case "o":
+                            opt_opap_anime = true;
+                            break;
+                        case "e":
+                            opt_show = true;
+                            break;
+                        default:
+                            if (args[i].StartsWith("--outdir-prefix="))
+                            {
+                                opt_outdir_prefix = args[i].Substring("--outdir-prefix=".Length);
+                            }
+                            else
+                            {
+                                PrintUsage();
+                                return 1;
+                            }
+                            break;
+                    }
+
+                }
+                else
+                {
+                    files_start_pos = i;
+                    break;
+                }
+            }
+
+
+
+            // ファイル指定部を読み込む
+            string[] files = args.Skip(files_start_pos).ToArray();
+
+
+            //指定されたファイルのうちpsdファイルを抽出かつフルパスに変換
+
+            List<string> psd_files = files
+                .Where(f => File.Exists(f) && Path.GetExtension(f).ToLower() == ".psd")
+                .Select(f => Path.GetFullPath(f))
+                .ToList();
+
+
+
+            //再帰的にpsdを検索して抽出
+            if (opt_recurse)
+            {
+                var dirs = files.Where(f => Directory.Exists(f));
+
+                foreach (var d in dirs)
+                {
+                    DirectoryInfo di = new DirectoryInfo(d);
+                    var rf = di.GetFiles("*.psd", SearchOption.AllDirectories)
+                            .Where(fi => !fi.Directory.Name.StartsWith(opt_outdir_prefix))
+                            .Select(fi => fi.FullName);
+                           
+                    psd_files.AddRange(rf);
+                    
+                }
+            }
+
+            //同一ファイルの重複を排除
+
+            List<string> psd_files_uniq = psd_files
+                    .Select(f => Path.GetFullPath(f))
+                    .Distinct()
+                    .ToList();
+
+
+            //処理を行う
+            psd_files_uniq.ForEach(f =>
+            {
+                Console.WriteLine(f);
+                PsdSplitter ps = new PsdSplitter(f);
+                ps.SplitAnimeLayer = opt_opap_anime;
+                ps.OutputDirPrefix = opt_outdir_prefix;
+                ps.VisibleTopLevelGroup = opt_show;
+                ps.Split();
+            });
+
+            return 0;
+        }
+
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("Usage: OpapPsdSplitter.exe [オプション]... [ファイル]...");
+            Console.WriteLine("PSDファイルをトップレベルのレイヤーグループ単位で分割します。");
+            Console.WriteLine("元のファイルには影響を与えません。");
+            Console.WriteLine();
+            Console.WriteLine("  -r     指定したディレクトリ及びサブディレクトリ内に存在する");
+            Console.WriteLine("         psdファイルを再帰的に検索して、すべて処理します。");
+            Console.WriteLine("  -o     OPAP-JP仕様のレイヤ構成において動画レイヤ（@マークで");
+            Console.WriteLine("         始まる名称のレイヤ）に含まれるサブレイヤグループを");
+            Console.WriteLine("         展開します。");
+            Console.WriteLine("  -s     トップレベルのレイヤグループが非表示状態になっている場合");
+            Console.WriteLine("         表示状態に変更する");
+            Console.WriteLine("  --outdir-prefix=PREFIX");
+            Console.WriteLine("         出力するフォルダ名の先頭に付けるプリフィクスを指定");
+            Console.WriteLine("         します。規定値は out- です。引用符は使用できません。");
+
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ OpapPsdSplitter/PsdSplitter.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhotoshopFile;
+using System.IO;
+
+
+namespace OpapPsdSplitter
+{
+    /// <summary>
+    /// PSDファイルを分割する
+    /// </summary>
+    public class PsdSplitter
+    {
+
+        /// <summary>
+        /// 入力ファイルパスを取得します
+        /// </summary>
+        public string InputFilePath { get; private set; }
+
+        /// <summary>
+        /// 入力PSDファイル内容を取得します
+        /// </summary>
+        public PsdFile InputPsdFile { get; private set; }
+
+
+        /// <summary>
+        /// 出力ディレクトリの先頭に付加するプリフィクスを取得または設定します
+        /// </summary>
+        public string OutputDirPrefix { get; set; }
+
+        /// <summary>
+        /// アニメーションレイヤグループ（「@」から始まるレイヤグループ）を展開するかどうかを取得または設定します
+        /// </summary>
+        public bool SplitAnimeLayer { get; set; }
+
+        /// <summary>
+        /// トップレベルグループを強制的に可視化するかどうかを取得または設定します
+        /// </summary>
+        public bool VisibleTopLevelGroup { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="filepath">入力PSDファイルパス</param>
+        public PsdSplitter(string filepath)
+        {
+            InputFilePath = Path.GetFullPath(filepath);
+            InputPsdFile =  new PsdFile();
+            OutputDirPrefix = "out-";
+
+            using (var fs = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+            {
+                InputPsdFile.Load(fs, Encoding.UTF8);
+            }
+        }
+
+
+        /// <summary>
+        /// PSDファイルをトップレベルのレイヤグループ単位で分割します
+        /// </summary>
+        /// <param name="outdir_prefix">出力先フォルダのプリフィクス</param>
+        /// <param name="split_anime_layer">アニメーションレイヤグループ（「@」から始まるレイヤグループ）の展開を許可します</param>
+        public void Split()
+        {
+
+            //出力先ディレクトリを作成する
+            string out_dir;
+
+            out_dir = Path.Combine(Path.GetDirectoryName(InputFilePath), OutputDirPrefix + Path.GetFileName(InputFilePath));
+            out_dir = Path.GetFullPath(out_dir);
+
+            if (!Directory.Exists(out_dir))
+            {
+                Directory.CreateDirectory(out_dir);  
+            }
+
+            //PSDを分解する
+            List<KeyValuePair<string, List<Layer>>> splitted_layers = SplitPsd();
+
+
+            //分解したPSDそれぞれ保存する
+            for (int i = 0; i < splitted_layers.Count; i++)
+            {
+
+                string name = splitted_layers[i].Key;
+                List<Layer> layers = splitted_layers[i].Value;
+
+                PsdFile psd_out = MakePsd(InputPsdFile, layers);
+
+                string out_filename = string.Format("{0}-{1:000}-{2}.psd", Path.GetFileNameWithoutExtension(InputFilePath), i,  name);
+                psd_out.Save(Path.Combine(out_dir, out_filename), Encoding.UTF8);
+
+            }
+
+
+
+        }
+
+
+
+        /// <summary>
+        /// PSDファイルをトップレベルのレイヤグループ単位で分割します
+        /// </summary>
+        /// <param name="psd_in">入力PSD</param>
+        /// <param name="split_anime_layer">アニメーションレイヤグループ（「@」から始まるレイヤグループ）の展開を許可します</param>
+        /// <returns>分割したレイヤオブジェクト</returns>
+        protected  List<KeyValuePair<string, List<Layer>>> SplitPsd()
+        {
+
+            const int DEPTH_THRESHOLD_DEFAULT = 0;
+            const int DEPTH_THRESHOLD_ANIMATE = 1;
+            string animate_layer_prefix = "@";
+
+
+            //結果格納用
+            List<KeyValuePair<string, List<Layer>>> sprittedLayers = new List<KeyValuePair<string, List<Layer>>>();
+
+
+
+            int depth = 0; //現在の階層の深さ
+            int depth_threshold = DEPTH_THRESHOLD_DEFAULT; //psdを分割する深さの閾値（初期値、第0階層）
+
+
+            Stack<string> layer_folder_path = new Stack<string>(); //レイヤのフォルダパス
+            Stack<List<Layer>> layer_list_stack = new Stack<List<Layer>>();
+
+
+            layer_list_stack.Push(new List<Layer>()); //第0階層のリストをpushしておく
+
+
+            // レイヤグループは逆順に並んでいるので逆転させて処理する
+            foreach (var layer in InputPsdFile.Layers.Reverse<Layer>())
+            {
+                List<Layer> current_layer_list = layer_list_stack.Peek();
+                current_layer_list.Add(layer);
+
+                LayerSectionInfo sec = layer.AdditionalInfo
+                    .OfType<LayerSectionInfo>().FirstOrDefault();
+
+                if (sec == null)
+                {
+                    continue;
+                }
+
+                switch (sec.SectionType)
+                {
+                    case LayerSectionType.OpenFolder:
+                    case LayerSectionType.ClosedFolder:
+                        //フォルダ開始
+
+                        if (SplitAnimeLayer
+                            && layer.Name.StartsWith(animate_layer_prefix)
+                            && depth == DEPTH_THRESHOLD_DEFAULT)
+                        {
+                            //動画レイヤグループとしての処理モードに入る
+
+                            depth_threshold = DEPTH_THRESHOLD_ANIMATE;
+
+                            //第1階層基準のレイヤリストを新規作成して以後これを使用
+                            current_layer_list = new List<Layer>();
+                            layer_list_stack.Push(current_layer_list);
+                        }
+
+                        depth++;
+                        layer_folder_path.Push(layer.Name);
+
+                        break;
+
+                    case LayerSectionType.SectionDivider:
+                        //フォルダ終了
+
+                        depth--;
+
+                        string close_folder_name = layer_folder_path.Pop();
+
+                        if (SplitAnimeLayer
+                            && close_folder_name.StartsWith(animate_layer_prefix)
+                            && depth == DEPTH_THRESHOLD_DEFAULT
+                            && depth_threshold == DEPTH_THRESHOLD_ANIMATE)
+                        {
+                            //通常処理モードに戻す
+                            depth_threshold = DEPTH_THRESHOLD_DEFAULT; //切り出し対象階層を第0階層に戻す
+
+                            //第0階層基準のレイヤリストに戻す
+                            layer_list_stack.Pop();
+                            current_layer_list = layer_list_stack.Peek();
+                        }
+
+                        break;
+                    default:
+                        //その他
+
+                        break;
+                }
+
+
+
+                if (depth == depth_threshold)
+                {
+
+                    if (CountNormalLayer(current_layer_list) > 0)
+                    {
+                        //通常レイヤが含まれている場合のみ出力する
+
+                        if (VisibleTopLevelGroup)
+                        {
+                            current_layer_list.First().Visible = true;
+                        }
+
+                        string name = MakeLayerPathString(layer_folder_path, current_layer_list.First().Name);
+                        List<Layer> out_layer_list = current_layer_list.Reverse<Layer>().ToList(); //逆順に戻す
+
+                        sprittedLayers.Add(new KeyValuePair<string, List<Layer>>(
+                                name,
+                                out_layer_list));
+                    }
+
+                    current_layer_list.Clear();
+                }
+
+            }
+
+            return sprittedLayers;
+
+        }
+
+        public static int CountNormalLayer(IEnumerable<Layer> layers)
+        {
+
+            //leyersのうち、フォルダ以外の通常レイヤをカウントする
+            int normal_layer_count = layers.Count((x) =>
+            {
+                LayerSectionInfo _sec = x.AdditionalInfo
+                    .OfType<LayerSectionInfo>().FirstOrDefault();
+
+                if (_sec == null)
+                {
+                    return true;
+                }
+
+                switch (_sec.SectionType)
+                {
+                    case LayerSectionType.OpenFolder:
+                    case LayerSectionType.ClosedFolder:
+                    case LayerSectionType.SectionDivider:
+
+                        return false;
+                }
+
+                return true;
+            });
+
+
+            return normal_layer_count;
+        }
+
+        public static string MakeLayerPathString(IEnumerable<string> folderPath, string name)
+        {
+
+            List<string> path = new List<string>();
+            path.AddRange(folderPath);
+            path.Add(name);
+
+
+            return string.Join("-", path);
+
+        }
+
+        public static PsdFile MakePsd(PsdFile basefile, IEnumerable<Layer> layers)
+        {
+
+            PsdFile psd = new PsdFile();
+            psd.ChannelCount = basefile.ChannelCount;
+            psd.ColorMode = basefile.ColorMode;
+            psd.BitDepth = basefile.BitDepth;
+            psd.RowCount = basefile.RowCount;
+            psd.ColumnCount = basefile.ColumnCount;
+            psd.ImageCompression = basefile.ImageCompression;
+            psd.Resolution = basefile.Resolution;
+
+            // 画像データのスペースを確保する
+            int imageSize = psd.RowCount * psd.ColumnCount;
+            psd.Layers.Clear();
+            for (short i = 0; i < psd.ChannelCount; i++)
+            {
+                var channel = new Channel(i, psd.BaseLayer);
+                channel.ImageData = new byte[imageSize];
+                channel.ImageCompression = psd.ImageCompression;
+                psd.BaseLayer.Channels.Add(channel);
+            }
+
+
+            psd.Layers.AddRange(layers);
+
+            return psd;
+
+        }
+    }
+}
